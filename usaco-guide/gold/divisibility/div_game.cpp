d4b4c20504affba5e8ced4d7c04c64864c7e0897
Rich-Nyan <93796523+Rich-Nyan@users.noreply.github.com>
2023-04-03 12:43:05 -0500

    Create div_game.cpp

---
+++ usaco-guide/gold/divisibility/div_game.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+	long long n;
+	cin>>n;
+	long long count = 0;
+	for (long long i = 2; i * i <= n; i ++)
+	{
+		long long div = 0;
+		while (n % i == 0)
+		{
+			div += 1;
+			n /= i;
+		}
+		if (div > 0)
+		{
+			long long subcnt = 1;
+			long long total = 0;
+			while (div >= subcnt)
+			{
+				total += 1;
+				subcnt += total + 1;
+			}
+			count += total;
+		}
+	}
+	if (n > 1)
+	{
+		count += 1;
+	}
+	cout<<count;
+}
