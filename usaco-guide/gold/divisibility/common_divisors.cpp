8240ef4255f212e61b9f8110b9ec2efdd34ca963
Rich-Nyan <93796523+Rich-Nyan@users.noreply.github.com>
2023-04-03 12:59:43 -0500

    Update common_divisors.cpp

--- usaco-guide/gold/divisibility/common_divisors.cpp
+++ usaco-guide/gold/divisibility/common_divisors.cpp
@@ -1,33 +1,42 @@
+/* Online C++ Compiler and Editor */
+#include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-	long long n;
-	cin>>n;
-	long long count = 0;
-	for (long long i = 2; i * i <= n; i ++)
-	{
-		long long div = 0;
-		while (n % i == 0)
-		{
-			div += 1;
-			n /= i;
-		}
-		if (div > 0)
-		{
-			long long subcnt = 1;
-			long long total = 0;
-			while (div >= subcnt)
-			{
-				total += 1;
-				subcnt += total + 1;
-			}
-			count += total;
-		}
-	}
-	if (n > 1)
-	{
-		count += 1;
-	}
-	cout<<count;
+int main()
+{
+   int n;
+   cin>>n;
+   
+   int a[n];
+   for (int i = 0; i < n; i++)
+   {
+       cin >> a[i];
+   }
+   
+   int total[1000001] = {0};
+   
+   for (int i = 0; i < n; i++)
+   {
+       for (int j = 1; j * j <= a[i]; j++)
+       {
+           if (a[i] % j == 0)
+           {
+                total[j] += 1;
+                if (j * j != a[i])
+                {
+                    total[a[i]/j] += 1;
+                }
+           }
+       }
+   }
+   
+   for (int i = 1000001; i > 0; i--)
+   {
+       if (total[i] >= 2)
+       {
+           cout<<i;
+           break;
+       }
+   }
 }
